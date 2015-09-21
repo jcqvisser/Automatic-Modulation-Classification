52b321e697c971c6b737a40efdc9ea93cdb5dbaa
Anthony Farquharson <anthonyjamesfarquharson@gmail.com>
2015-09-21 15:23:01 +0200

    Comments for UHD Read and Shared Buffer, added them to the source (src) folder.

---
+++ src/uhdread.cpp
@@ -0,0 +1,119 @@
+#include "uhdread.h"
+
+UhdRead::UhdRead(double rate, double freq, double gain, size_t frameSize, std::string args) :
+    _usrp(uhd::usrp::multi_usrp::make(args)),
+    _rxStream(nullptr),
+    _rxMetadata(),
+    _frameSize(frameSize),
+    _buffer(new SharedBuffer() ),
+    _uhdThread(),
+    _isReading(false)
+{
+    init(rate, freq, gain);
+}
+
+void UhdRead::setThreadPrioritySafe()
+{
+    uhd::set_thread_priority_safe();
+}
+
+void UhdRead::init(double rate, double freq, double gain)
+{
+    // Set the clock source:
+    _usrp->set_clock_source("internal");
+
+    // Set the sample rate:
+    std::cout << std::endl
+              << boost::format("Setting RX Rate: %f Msps...") % (rate/1e6)
+              << std::endl;
+    _usrp->set_rx_rate(rate);
+    std::cout << boost::format("Actual RX Rate: %f Msps...") % (_usrp->get_rx_rate()/1e6)
+              << std::endl << std::endl;
+
+    // Set the center frequency:
+    std::cout << boost::format("Setting RX Freq: %f MHz...") % (freq/1e6)
+              << std::endl;
+    uhd::tune_request_t tune_request(freq);
+    _usrp->set_rx_freq(tune_request);
+    std::cout << boost::format("Actual RX Freq: %f MHz...") % (_usrp->get_rx_freq()/1e6)
+              << std::endl << std::endl;
+
+    // Set the gain:
+    std::cout << boost::format("Setting RX Gain: %f dB...") % gain
+              << std::endl;
+    _usrp->set_rx_gain(gain);
+    std::cout << boost::format("Actual RX Gain: %f dB...") % _usrp->get_rx_gain()
+              << std::endl << std::endl;
+
+    // Configure the stream:
+    uhd::stream_args_t stream_args("fc64"); //complex floats
+    uhd::rx_streamer::sptr rx_stream = _usrp->get_rx_stream(stream_args);
+    uhd::stream_cmd_t stream_cmd(uhd::stream_cmd_t::STREAM_MODE_START_CONTINUOUS);
+    stream_cmd.stream_now = true;
+    rx_stream->issue_stream_cmd(stream_cmd);
+
+}
+
+void UhdRead::startStream()
+{
+    // Start the data stream
+    uhd::stream_args_t stream_args("fc64"); // Complex doubles
+    _rxStream = _usrp->get_rx_stream(stream_args);
+    uhd::stream_cmd_t stream_cmd(uhd::stream_cmd_t::STREAM_MODE_START_CONTINUOUS);
+    stream_cmd.stream_now = true;
+    _rxStream->issue_stream_cmd(stream_cmd);
+    _isReading = true;
+
+    // Allow usrp setup time.
+    boost::this_thread::sleep(boost::posix_time::seconds(1.00));
+
+    // Launch thread
+    _uhdThread = boost::thread(&UhdRead::run, this);
+}
+
+void UhdRead::stopStream()
+{
+    // Stop data stream
+    uhd::stream_cmd_t stream_cmd(uhd::stream_cmd_t::STREAM_MODE_STOP_CONTINUOUS);
+    stream_cmd.stream_now = false;
+    _rxStream->issue_stream_cmd(stream_cmd);
+
+    _isReading = false;
+
+    _uhdThread.join();
+}
+
+void UhdRead::run()
+{
+    size_t numAcceptedSamples = 0;
+    size_t numRxSamples = 0;
+    std::vector< std::complex<double> > tempBuffer(_frameSize);
+    while(_isReading)
+    {
+        numAcceptedSamples = 0;
+        numRxSamples = 0;
+
+        while(numAcceptedSamples < _frameSize)
+        {
+            numRxSamples = _rxStream->recv(&tempBuffer.front(), _frameSize, _rxMetadata);
+            numAcceptedSamples += numRxSamples;
+        }
+
+        // Get unique access.
+        boost::shared_ptr < boost::shared_mutex > mutex = _buffer->getMutex();
+        boost::unique_lock < boost::shared_mutex > lock (*mutex.get());
+        for(std::complex<double> & tempSample : tempBuffer)
+        {
+            _buffer->push_back(tempSample);
+            tempSample = 0;
+        }
+    }
+    std::cout << std::endl << "UHD Reader thread closing... " << std::endl;
+}
+
+boost::shared_ptr< SharedBuffer > UhdRead::getBuffer()
+{
+    return _buffer;
+}
+
+
---
+++ src/uhdread.h
@@ -0,0 +1,87 @@
+#ifndef UHDREAD_H
+#define UHDREAD_H
+
+#include <uhd.h>
+#include <uhd/types/tune_request.hpp>
+#include <uhd/utils/thread_priority.hpp>
+#include <uhd/usrp/multi_usrp.hpp>
+#include <uhd/exception.hpp>
+
+#include <boost/thread.hpp>
+#include <boost/format.hpp>
+#include <boost/smart_ptr.hpp>
+#include "sharedbuffer.h"
+
+/**
+ * @brief The UhdRead class
+ * The UHD read class is used for getting data from the UHD and putting it into a buffer,
+ * the buffer is of type SharedBuffer, which has mutex for threading protection. The class
+ * can return a boost shared pointer to the buffer object, for access from other classes.
+ * The UHD read class is threaded, the thread is started with the startStream() function, and stopped
+ * with the stopStream() function.
+ * This function will mutex lock the buffer when writing information to it.
+ *
+ * @author Anthony Farquharson - 563648
+ * @author Jacques Visser - 457817
+ */
+
+class UhdRead
+{
+public:
+    /**
+     * @brief The overloaded constructor takes in the variables required for the set up of the USRP.
+     * @param rate The sample rate of the USRP.
+     * @param freq The center frequency of the USRP
+     * @param gain The gain of the USRP.
+     * @param frameSize The size of the frame requested.
+     * @param args Any additional arguments for the USRP.
+     */
+    explicit UhdRead(double rate, double freq, double gain, size_t frameSize = 384, std::string args = "");
+
+    /**
+     * @brief startStream The start stream function will launch the thread for the run loop.
+     */
+    void startStream();
+
+    /**
+     * @brief The stopStream function will join the thread with the main thread again, closing the USRP
+     * stream and stoping the threads main function loop.
+     */
+    void stopStream();
+
+    /**
+     * @brief The getBuffer function will return a boost shared pointer to the buffer object.
+     * @return This function returns the pointer to the buffer object.
+     */
+    boost::shared_ptr< SharedBuffer > getBuffer();
+
+    /**
+     * @brief The setThreadPrioritySafe function is static, used for setting USRP thread priority.
+     */
+    static void setThreadPrioritySafe();
+
+private:
+    /**
+     * @brief The init function performs the initialization of the USRP, called by the constructor.
+     * @param rate The sample rate of the USRP.
+     * @param freq The center frequency of the USRP
+     * @param gain The gain of the USRP.
+     */
+    void init(double rate, double freq, double gain);
+
+    /**
+     * @brief The main run loop that will run on a particular thread.
+     */
+    void run();
+
+    uhd::usrp::multi_usrp::sptr _usrp;
+    uhd::rx_streamer::sptr _rxStream;
+    uhd::rx_metadata_t _rxMetadata;
+    size_t _frameSize;
+    boost::shared_ptr< SharedBuffer > _buffer;
+    boost::thread _uhdThread;
+    volatile bool _isReading;
+    boost::shared_mutex _bufferMutex;
+};
+
+#endif // UHDREAD_H
