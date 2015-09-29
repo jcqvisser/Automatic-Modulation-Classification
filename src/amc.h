980d13549233512fcdac30aa6df366b66f3e46e7
jcqvisser@gmail.com <jcqvisser@gmail.com>
2015-09-29 13:19:18 +0200

    Updated AMC and FeatureExtractor in src

--- src/amc.h
+++ src/amc.h
@@ -19,25 +19,21 @@ namespace AMC
             std::vector<std::complex<double> > &x);
     std::vector<std::complex<double> > ifft(
             std::vector<std::complex<double> > &X);
-    std::vector<std::complex<double> > instantaneousSignal(
-            std::vector<std::complex<double> > &x);
-    std::vector<double> instantaneousAmplitude(
-            std::vector<std::complex<double> > &x);
-    std::vector<double> instantaneousPhase(
-            std::vector<std::complex<double> > &x);
+
     std::vector<double> unwrapPhase(
             std::vector<double> x_i_phase);
-    std::vector<double> unwrappedInstantaneousPhase(
-            std::vector<std::complex<double> > &x);
-    std::vector<double> nonLinearUnwrappedInstantaneousPhase(
-            std::vector<std::complex<double> > &x,
-            const double &fc,
-            const double &fs);
+
+
+    auto phase(const std::vector<std::complex<double> > &x) -> std::vector<double>;
+    auto removeLinearPhase(const std::vector<double> &x, size_t fcn) -> std::vector<double>;
+
 
     auto abs(std::vector<double> x) -> std::vector<double>;
     auto abs(std::vector<std::complex<double> > x) -> std::vector<double>;
+
     auto mean(const std::vector<std::complex<double> > &x) -> std::complex<double>;
     auto mean(const std::vector<double> &x) -> double;
+
     auto stdDev(const std::vector<double> &x) -> double;
     auto stdDev(const std::vector<std::complex<double> > &x) -> double;
     void stdDevKurtosis(
@@ -48,18 +44,24 @@ namespace AMC
             const std::vector<std::complex<double> > &x,
             double &stdDev,
             double &kurt);
-    auto differentiate(const std::vector<double> &x, const double &fs) -> std::vector<double>;
+    auto differentiate(const std::vector<double> &x) -> std::vector<double>;
+
+    //remove
     auto absMax(const std::vector<double> &x) -> double;
     auto absMax(const std::vector<std::complex<double> > &x) -> double;
-    auto max(const std::vector<std::complex<double> > &x) -> std::complex<double>;
-    auto center(std::vector<double> x) -> std::vector<double>;
-    auto center(std::vector<std::complex<double> > x) -> std::vector<std::complex<double> >;
-    auto normalize(std::vector<double> x) -> std::vector<double>;
-    auto normalize(std::vector<std::complex<double> > x) -> std::vector<std::complex<double> >;
 
-    auto symmetry(const std::vector<std::complex<double> > &x, const double &fc, const double &fs) -> double;
-    auto maxPower(const std::vector<double> &x) -> double;
-    auto maxPower(const std::vector<std::complex<double> > &x) -> double;
+    auto symmetry(const std::vector<std::complex<double> > &x, const size_t &fcn) -> double;
+
+    auto maxPower(const std::vector<double> &x, size_t &k) -> double;
+    auto maxPower(const std::vector<std::complex<double> > &x, size_t &k) -> double;
+
+    auto center(const std::vector<double> &x) -> std::vector<double>;
+    auto center(const std::vector<std::complex<double> > &x) -> std::vector<std::complex<double> >;
+
+    auto normalize(const std::vector<double> &x) -> std::vector<double>;
+    auto normalize(const std::vector<std::complex<double> > &x) -> std::vector<std::complex<double> >;
+
+    auto removeNegFreq(const std::vector<std::complex<double> > &x) -> std::vector<std::complex<double> >;
 }
 
 #endif
