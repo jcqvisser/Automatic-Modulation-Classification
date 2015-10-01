a9697749296ea2b775b68256328afe6c6d1b27ff
Anthony Farquharson <anthonyjamesfarquharson@gmail.com>
2015-10-01 10:54:28 +0200

    Added digital demod objects to src folder.

---
+++ src/demodulators/digitaldemod.cpp
@@ -0,0 +1,53 @@
+#include "digitaldemod.h"
+
+DigitalDemod::DigitalDemod(AmcDemodulator * func, double rel_fs, double rel_fc) :
+    _func(func),
+    _rel_fc(rel_fc),
+    _rel_fs(rel_fs),
+    _rel_tau(1/_rel_fs),
+    _pi(atan(1) * 4),
+    _symbol(0.0, 0.0),
+    _realSymbol(0.0),
+    _imagSymbol(0.0),
+    _t(0)
+{
+
+}
+
+double DigitalDemod::demod(const std::complex<double> &sampleData)
+{
+    // TODO:: Implement some phase correction.
+
+    // Every symbol number of samples, reset time counter.
+    if(_t == _rel_tau)
+    {
+        _t = 0;
+    }
+
+    // Get real and imaginary components at height of symbol.
+    if(_t == _rel_tau / 2)
+    {
+        _realSymbol = sampleData.real() * cos(2 * _pi * _rel_fc * _t);
+    }
+    if(_t == (_rel_tau / 2 + ceil((1/_rel_fc) / 4)))
+    {
+        _imagSymbol = sampleData.imag() * sin(2 * _pi * _rel_fc * _t);
+        _symbol = std::complex<double>(_realSymbol, _imagSymbol);
+    }
+
+    // Increment time counter.
+    ++_t;
+
+    return _func->demod(_symbol);
+}
+
+void DigitalDemod::reset()
+{
+    _t = 0;
+    _func->reset();
+}
+
+std::string DigitalDemod::modType()
+{
+    return _func->modType();
+}
---
+++ src/demodulators/digitaldemod.h
@@ -0,0 +1,58 @@
+#ifndef DIGITALDEMOD_H
+#define DIGITALDEMOD_H
+
+#include <boost/smart_ptr.hpp>
+#include <iostream>
+#include <cmath>
+#include "amcdemodulator.h"
+
+/**
+ * @brief The DigitalDemod class performs demodulation of digital signals, that have been
+ * modulated using a particular scheme, the symbols are demodulated using the function passed
+ * into this class.
+ */
+
+class DigitalDemod : public AmcDemodulator
+{
+public:
+    /**
+     * @brief DigitalDemod constructor, configures the Digital demodulator, with information such as
+     * carrier frequency and symbol rate, as well as the modulation scheme demodulator class (MPSK, MQAM, etc.).
+     * @param The demodulation scheme to use, must be digital (MPSK, MQAM, etc.).
+     * @param The relative symbol rate, fs/Fs (Symbol rate / sample rate).
+     * @param The relative carrier frequency, fc / Fs (Carrier Freq / sample rate).
+     */
+    explicit DigitalDemod(AmcDemodulator * func, double rel_fs, double rel_fc);
+
+    /**
+     * @brief Performs demodulation on a particular data point, state information is stored within this object
+     * so this must be called in order.
+     * @param The data point to be demodulated.
+     * @return The demodulated symbol.
+     */
+    double demod(const std::complex<double> &sampleData);
+
+    /**
+     * @brief Reset the state information within this object.
+     */
+    void reset();
+
+    /**
+     * @brief Returns the modulation type that is being demodulated.
+     * @return Modulation Type (string).
+     */
+    std::string modType();
+
+private:
+    boost::scoped_ptr<AmcDemodulator> _func;
+    double _rel_fc;
+    double _rel_fs;
+    double _rel_tau;
+    double _pi;
+    std::complex <double> _symbol;
+    double _realSymbol;
+    double _imagSymbol;
+    size_t _t;
+};
+
+#endif // DIGITALDEMOD_H
